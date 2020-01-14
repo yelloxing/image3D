cfac2c9cb5d99cc623629e717f1848bd07a1c7fb
paper520 <3294539252@qq.com>
2020-01-14 13:09:25 +0800

    开发新功能（照相机），待添加测试

--- build/image3D.js
+++ build/image3D.js
@@ -11,7 +11,7 @@
 * Copyright yelloxing
 * Released under the MIT license
 *
-* Date:Mon Jan 13 2020 19:26:23 GMT+0800 (GMT+08:00)
+* Date:Tue Jan 14 2020 13:08:04 GMT+0800 (GMT+08:00)
 */
 
 'use strict';
@@ -553,10 +553,214 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     }
 
     /**
+     * 在(a,b,c)方向位移d
+     * @private
+     */
+    function _move(d, a, b, c) {
+        c = c || 0;
+        var sqrt = Math.sqrt(a * a + b * b + c * c);
+        return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, a * d / sqrt, b * d / sqrt, c * d / sqrt, 1];
+    }
+
+    /**
+     * 围绕0Z轴旋转
+     * 其它的旋转可以借助transform实现
+     * 旋转角度单位采用弧度制
+     * 
+     * @private
+     */
+    function _rotate(deg) {
+        var sin = Math.sin(deg),
+            cos = Math.cos(deg);
+        return [cos, sin, 0, 0, -sin, cos, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+    }
+
+    /**
+     * 围绕圆心x、y和z分别缩放xTimes, yTimes和zTimes倍
+     * 
+     * @private
+     */
+    function _scale(xTimes, yTimes, zTimes, cx, cy, cz) {
+        cx = cx || 0;cy = cy || 0;cz = cz || 0;
+        return [xTimes, 0, 0, 0, 0, yTimes, 0, 0, 0, 0, zTimes, 0, cx - cx * xTimes, cy - cy * yTimes, cz - cz * zTimes, 1];
+    }
+
+    /**
+     * 针对任意射线(a1,b1,c1)->(a2,b2,c2)
+     * 计算出二个变换矩阵
+     * 分别为：任意射线变成OZ轴变换矩阵 + OZ轴变回原来的射线的变换矩阵
+     * 
+     * @private
+     */
+    function _transform(a1, b1, c1, a2, b2, c2) {
+
+        if (typeof a1 === 'number' && typeof b1 === 'number') {
+
+            // 如果设置二个点
+            // 表示二维上围绕某个点旋转
+            if (typeof c1 !== 'number') {
+                c1 = 0;a2 = a1;b2 = b1;c2 = 1;
+            }
+            // 只设置三个点(设置不足六个点都认为只设置了三个点)
+            // 表示围绕从原点出发的射线旋转
+            else if (typeof a2 !== 'number' || typeof b2 !== 'number' || typeof c2 !== 'number') {
+                    a2 = a1;b2 = b1;c2 = c1;a1 = 0;b1 = 0;c1 = 0;
+                }
+
+            if (a1 == a2 && b1 == b2 && c1 == c2) throw new Error('It\'s not a legitimate ray!');
+
+            var sqrt1 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1)),
+                cos1 = sqrt1 != 0 ? (b2 - b1) / sqrt1 : 1,
+                sin1 = sqrt1 != 0 ? (a2 - a1) / sqrt1 : 0,
+                b = (a2 - a1) * sin1 + (b2 - b1) * cos1,
+                c = c2 - c1,
+                sqrt2 = Math.sqrt(b * b + c * c),
+                cos2 = sqrt2 != 0 ? c / sqrt2 : 1,
+                sin2 = sqrt2 != 0 ? b / sqrt2 : 0;
+
+            return [
+
+            // 任意射线变成OZ轴变换矩阵
+            [cos1, cos2 * sin1, sin1 * sin2, 0, -sin1, cos1 * cos2, cos1 * sin2, 0, 0, -sin2, cos2, 0, b1 * sin1 - a1 * cos1, c1 * sin2 - a1 * sin1 * cos2 - b1 * cos1 * cos2, -a1 * sin1 * sin2 - b1 * cos1 * sin2 - c1 * cos2, 1],
+
+            // OZ轴变回原来的射线的变换矩阵
+            [cos1, -sin1, 0, 0, cos2 * sin1, cos2 * cos1, -sin2, 0, sin1 * sin2, cos1 * sin2, cos2, 0, a1, b1, c1, 1]];
+        } else {
+            throw new Error('a1 and b1 is required!');
+        }
+    }
+
+    // 二个4x4矩阵相乘
+    // 或矩阵和齐次坐标相乘
+    var _multiply = function _multiply(matrix4, param) {
+        var newParam = [];
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < param.length / 4; j++) {
+                newParam[j * 4 + i] = matrix4[i] * param[j * 4] + matrix4[i + 4] * param[j * 4 + 1] + matrix4[i + 8] * param[j * 4 + 2] + matrix4[i + 12] * param[j * 4 + 3];
+            }
+        }return newParam;
+    };
+
+    /**
+     * 4x4矩阵
+     * 列主序存储
+     * @since V0.2.0
+     * @public
+     */
+    function Matrix4(initMatrix4) {
+
+        var matrix4 = initMatrix4 || [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+
+        var matrix4Obj = {
+
+            // 移动
+            "move": function move(dis, a, b, c) {
+                matrix4 = _multiply(_move(dis, a, b, c), matrix4);
+                return matrix4Obj;
+            },
+
+            // 旋转
+            "rotate": function rotate(deg, a1, b1, c1, a2, b2, c2) {
+                var matrix4s = _transform(a1, b1, c1, a2, b2, c2);
+                matrix4 = _multiply(_multiply(_multiply(matrix4s[1], _rotate(deg)), matrix4s[0]), matrix4);
+                return matrix4Obj;
+            },
+
+            // 缩放
+            "scale": function scale(xTimes, yTimes, zTimes, cx, cy, cz) {
+                matrix4 = _multiply(_scale(xTimes, yTimes, zTimes, cx, cy, cz), matrix4);
+                return matrix4Obj;
+            },
+
+            // 乘法
+            // 可以传入一个矩阵(matrix4,flag)
+            "multiply": function multiply(newMatrix4, flag) {
+                matrix4 = flag ? _multiply(matrix4, newMatrix4) : _multiply(newMatrix4, matrix4);
+                return matrix4Obj;
+            },
+
+            // 对一个坐标应用变换
+            // 齐次坐标(x,y,z,w)
+            "use": function use(x, y, z, w) {
+                // w为0表示点位于无穷远处，忽略
+                z = z || 0;w = w || 1;
+                var temp = _multiply(matrix4, [x, y, z, w]);
+                temp[0] = +temp[0].toFixed(7);
+                temp[1] = +temp[1].toFixed(7);
+                temp[2] = +temp[2].toFixed(7);
+                temp[3] = +temp[3].toFixed(7);
+                return temp;
+            },
+
+            // 矩阵的值
+            "value": function value() {
+                return matrix4;
+            }
+
+        };
+
+        return matrix4Obj;
+    }
+
+    /**
      * 照相机
      * -------------------------
      */
-    function $Camera(CORE, CONFIG) {}
+    function $Camera(CORE, CONFIG) {
+
+        return function () {
+
+            return new function Camera() {
+                var _this3 = this;
+
+                // 摄像头位置改变和物体位置改变矩阵初始化
+                var matrix4 = Matrix4();
+
+                /**
+                 * 摄像头位置改变
+                 */
+
+                // 旋转
+                this.rotateEye = function (deg, a1, b1, c1, a2, b2, c2) {
+                    matrix4.rotate(-deg, a1, b1, c1, a2, b2, c2);
+                    return _this3;
+                };
+
+                // 移动
+                this.moveEye = function (dis, a, b, c) {
+                    matrix4.move(-dis, a, b, c);
+                    return _this3;
+                };
+
+                /**
+                 * 物体位置改变
+                 */
+
+                // 旋转
+                this.rotateBody = function (deg, a1, b1, c1, a2, b2, c2) {
+                    matrix4.rotate(deg, a1, b1, c1, a2, b2, c2);
+                    return _this3;
+                };
+
+                // 移动
+                this.moveBody = function (dis, a, b, c) {
+                    matrix4.move(dis, a, b, c);
+                    return _this3;
+                };
+
+                // 缩放
+                this.scaleBody = function (xTimes, yTimes, zTimes, cx, cy, cz) {
+                    matrix4.scale(xTimes, yTimes, zTimes, cx, cy, cz);
+                    return _this3;
+                };
+
+                // 获取当前的变换矩阵值
+                this.value = function () {
+                    return matrix4.value();
+                };
+            }();
+        };
+    }
 
     /**
      * 常见的图形数据计算
@@ -682,7 +886,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         return function (unit) {
 
             return new function Texture2D() {
-                var _this3 = this;
+                var _this4 = this;
 
                 // 创建纹理
                 var texture = CORE.texture('2d', unit);
@@ -690,7 +894,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
                 // 绑定图片
                 this.write = function (img) {
                     texture.useImage(img);
-                    return _this3;
+                    return _this4;
                 };
             }();
         };
@@ -705,7 +909,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         return function (width, height) {
 
             return new function TextureCube() {
-                var _this4 = this;
+                var _this5 = this;
 
                 // 创建纹理
                 var texture = CORE.texture('cube');
@@ -713,7 +917,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
                 // 绑定图片
                 this.write = function (img1, img2, img3, img4, img5, img6) {
                     texture.useCube([img1, img2, img3, img4, img5, img6], width, height);
-                    return _this4;
+                    return _this5;
                 };
             }();
         };
--- src/Object/camera/index.js
+++ src/Object/camera/index.js
@@ -1,7 +1,61 @@
+import Matrix4 from "@yelloxing/core.js/tools/Matrix4";
+
 /**
  * 照相机
  * -------------------------
  */
 export default function (CORE, CONFIG) {
+
+    return function () {
+
+        return new function Camera() {
+
+            // 摄像头位置改变和物体位置改变矩阵初始化
+            let matrix4 = Matrix4();
+
+            /**
+             * 摄像头位置改变
+             */
+
+            // 旋转
+            this.rotateEye = (deg, a1, b1, c1, a2, b2, c2) => {
+                matrix4.rotate(-deg, a1, b1, c1, a2, b2, c2);
+                return this;
+            };
+
+            // 移动
+            this.moveEye = (dis, a, b, c) => {
+                matrix4.move(-dis, a, b, c);
+                return this;
+            };
+
+            /**
+             * 物体位置改变
+             */
+
+            // 旋转
+            this.rotateBody = (deg, a1, b1, c1, a2, b2, c2) => {
+                matrix4.rotate(deg, a1, b1, c1, a2, b2, c2);
+                return this;
+            };
+
+            // 移动
+            this.moveBody = (dis, a, b, c) => {
+                matrix4.move(dis, a, b, c);
+                return this;
+            };
+
+            // 缩放
+            this.scaleBody = (xTimes, yTimes, zTimes, cx, cy, cz) => {
+                matrix4.scale(xTimes, yTimes, zTimes, cx, cy, cz);
+                return this;
+            };
+
+            // 获取当前的变换矩阵值
+            this.value = () => matrix4.value();
+
+        };
+
+    };
 
 };
